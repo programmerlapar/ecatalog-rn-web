d411d489e882e2d25c9c61577d990a2c95dec62f
Firmansyah Otoluwa <pirmansah.otoluwa@gmail.com>
2026-08-27 19:33:49 +0700

    CCA-15 fix About Cafe screen

--- screen/About.js
+++ screen/About.js
@@ -1,21 +1,43 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 
 const About = () => {
     return (
-        <View style={{flex: 1,}}>
-            <Text>AboutScreen</Text>
-            <Text>AboutScreen</Text>
-            <Text>AboutScreen</Text>
-            <Text>AboutScreen</Text>
-            <Text>AboutScreen</Text>
-            <Text>AboutScreen</Text>
-            <Text>AboutScreen</Text>
-            <Text>AboutScreen</Text>
+        <View style={styles.container}>
+            <Image source={require('../assets/cafe.png')} style={styles.logo} />
+            <Text style={styles.title}>Snow Motion Cafe</Text>
+            <Text style={styles.description}>
+                Snow Motion Cafe adalah cafe dengan vibe modern dan desain interior
+                yang kece banget. Didirikan pada tahun 2020.
+            </Text>
         </View>
     )
 }
 
 export default About;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 24,
+    },
+    logo: {
+        width: 120,
+        height: 120,
+        marginBottom: 16,
+    },
+    title: {
+        color: 'gold',
+        fontSize: 24,
+        fontStyle: 'italic',
+        fontWeight: 'bold',
+        marginBottom: 16,
+    },
+    description: {
+        maxWidth: 480,
+        textAlign: 'center',
+        lineHeight: 24,
+    },
+})
