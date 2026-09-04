4d78d986f40140c377a640a5061e40b5a863c53d
Firmansyah Otoluwa <pirmansah.otoluwa@gmail.com>
2026-09-04 19:10:50 +0700

    CCA-129 restore web navigation contrast

--- components/MenuBar.js
+++ components/MenuBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Ionicons } from "@expo/vector-icons";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { DarkAccent, MutedTextColor, SurfaceColor } from "../constant/ColorsConst";
 
 const MenuBar = ({ menuHandler, _rem, isWeb, title, fontSize }) => {
   return (
@@ -12,10 +13,18 @@ const MenuBar = ({ menuHandler, _rem, isWeb, title, fontSize }) => {
       style={[styles.button, { minWidth: isWeb ? 112 : 190 }]}
     >
       <View style={styles.content}>
-        <Text style={[styles.text, { fontSize: fontSize || (isWeb ? _rem(10) : _rem(8)) }]}>
+        <Text
+          style={[
+            styles.text,
+            {
+              color: isWeb ? SurfaceColor : DarkAccent,
+              fontSize: fontSize || (isWeb ? _rem(10) : _rem(8)),
+            },
+          ]}
+        >
           {title}
         </Text>
-        <Ionicons name="chevron-forward" size={16} color="#6d7b76" />
+        <Ionicons name="chevron-forward" size={16} color={isWeb ? SurfaceColor : MutedTextColor} />
       </View>
     </TouchableOpacity>
   );
